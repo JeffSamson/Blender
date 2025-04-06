44d891b09eefa859899c3b414359e3657e1bdcc7
Jeff Samson <jeff.samson@outlook.com>
2025-04-06 12:18:30 -0700

    adding vector sand box file

--- Python/remove-unused-nodes.py
+++ Python/remove-unused-nodes.py
@@ -1,5 +1,6 @@
 import bpy
+
 # Loop through all node groups and delete those not in use
 for node_group in bpy.data.node_groups:
-    if len(node_group.users) == 0:
+    if node_group.users == 0:  # Directly compare users to 0
         bpy.data.node_groups.remove(node_group)
